8e3fc0869d0404b4f259396b3934e3929a46c26e
Eduard Mazo <eduard.mazo@gmail.com>
2018-07-19 09:00:08 -0500

    :Whale: highlighting code

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,53 @@
-<template lang="pug">
-#app
-  p {{shoW ? 'Text Hide' : 'Form Hide'}}
-  .container(v-show="shoW")
-    nav.level
-      p.level-item.has-text-centered
-        img(src='./assets/img1.jpg' class="-responsive")
-    nav.level
-      h2
-      strong.level-item.has-text-centered.-tittle Registro estación Platzi 
-    nav
-      label(for="name" class="-label") Nombre
-      input(v-model="name" type="text" autocomplete="off" id="name"  class="-input") 
-    nav
-      label(for="name" class="-label") Apellido
-      input(v-model="lastName" type="text" autocomplete="off" id="lastName" class="-input") 
-    nav
-      label(for="name" class="-label") Birthday
-      input(v-model="year" type="text" autocomplete="off" id="year" class="-input") 
-    nav.-btn
-      a.button(@click="doIt") Hide
-    p {{birthDay}}
-    .field.has-addons
-      .control
-        input.input(v-model="msg" type='text')
-      .control
-        a.button.is-info(@click="replaceString") Search 
-    p {{msg1}}  
+<template>
+<div id="app" class="wrapp">
+<div id="mySidenav" class="sidenav">
+  <a class="closebtn">&times;</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+  <div class="body">
+     <pre>
+        <code class="json">
+          {
+            "experience": [
+              {
+                "year": "2017",
+                "job": "EPM",
+                "position": "Electronic Engineer"
+              },   
+              {
+                "year": "2011",
+                "job": "FUdeA",
+                "position": "Maniobrita"
+              },
+              {
+                "year": "2009",
+                "job": "FUdeA",
+                "position": "Train driver"
+              }
+            ]
+          }
+        </code>
+      </pre>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
+import hljs from "highlight.js";
+hljs.initHighlightingOnLoad();
+
 export default {
   name: "app",
   data() {
-    return {
-      msg: "",
-      msg1: "",
-      shoW: true,
-      name: "",
-      lastName: "",
-      year: "",
-      url: "https://www.google.com.co"
-    };
+    return {};
   },
-  methods: {
-    doIt() {
-      this.shoW = false;
-    },
-    replaceString() {
-      this.msg1 = this.msg.split(" ").join("-");
-      this.name = "";
-      this.lastName = "";
-      this.year = "";
-    }
-  },
-  computed: {
-    birthDay() {
-      if (this.year.length == 4) {
-        return `${this.name} ${this.lastName} tiene ${2018 - this.year} años`;
-      }
-    }
-  }
+  methods: {},
+  computed: {}
 };
 </script>
 
