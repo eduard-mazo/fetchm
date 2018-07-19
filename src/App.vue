fe6082e38fb472e501709886d37dbb96038db53a
Eduard Mazo <eduard.mazo@gmail.com>
2018-07-19 12:55:44 -0500

    :hurtrealbad: Undertanding lifecicle

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <template lang="pug">
 #app
-  p {{shoW ? 'Text Hide' : 'Form Hide'}}
-  .container(v-show="shoW")
-    nav.level
-      p.level-item.has-text-centered
-        img(src='./assets/img1.jpg' class="-responsive")
-    nav.level
-      h2
-      strong.level-item.has-text-centered.-tittle Registro estación Platzi 
-    nav
-      label(for="name" class="-label") Nombre
-      input(v-model="name" type="text" autocomplete="off" id="name"  class="-input") 
-    nav
-      label(for="name" class="-label") Apellido
-      input(v-model="lastName" type="text" autocomplete="off" id="lastName" class="-input") 
-    nav
-      label(for="name" class="-label") Birthday
-      input(v-model="year" type="text" autocomplete="off" id="year" class="-input") 
-    nav.-btn
-      a.button(@click="doIt") Hide
-    p {{birthDay}}
-    .field.has-addons
-      .control
-        input.input(v-model="msg" type='text')
-      .control
-        a.button.is-info(@click="replaceString") Search 
-    p {{msg1}}  
+  img(src='./assets/logo.png' class="-responsive")
+  h1 Ciclo de vida
+  p {{person}}
+
 </template>
 
 <script>
@@ -33,33 +11,17 @@ export default {
   name: "app",
   data() {
     return {
-      msg: "",
-      msg1: "",
-      shoW: true,
-      name: "",
-      lastName: "",
-      year: "",
-      url: "https://www.google.com.co"
+      person: "eduard"
     };
   },
-  methods: {
-    doIt() {
-      this.shoW = false;
-    },
-    replaceString() {
-      this.msg1 = this.msg.split(" ").join("-");
-      this.name = "";
-      this.lastName = "";
-      this.year = "";
-    }
+  created() {
+    console.log("Created ..");
   },
-  computed: {
-    birthDay() {
-      if (this.year.length == 4) {
-        return `${this.name} ${this.lastName} tiene ${2018 - this.year} años`;
-      }
-    }
-  }
+  mounted() {
+    console.log("mounted ..");
+  },
+  methods: {},
+  computed: {}
 };
 </script>
 
